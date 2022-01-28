83e01e6d297ec8f964bad89213b5587f372dec92
bealabea <beatricemarin89@yahoo.it>
2022-01-28 15:49:27 +0100

    Adds a cards component (nested in main)

---
+++ src/components/CardsContainer.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="main-container">
+        <div v-for="(card, index) in cards" :key="index" class="cards">
+            <div class="card-image">
+                <img :src="card.thumb" alt="">
+            </div>
+            <div>{{card.series}}</div>
+        </div> 
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        cards: Array
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/style/variables.scss';
+@import '@/style/cards-container.scss';
+</style>
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,14 +1,7 @@
 <template>
     <main>
         <div class="section-title"><h1>current series</h1></div>
-        <div class="main-container">
-            <div v-for="(card, index) in cards" :key="index" class="cards">
-                <div class="card-image">
-                    <img :src="card.thumb" alt="">
-                </div>
-                <div>{{card.series}}</div>
-            </div> 
-        </div>
+        <cards-container :cards="cards"/>
         <div class="button-cards">
             <button> LOAD MORE</button>
         </div>
@@ -16,7 +9,11 @@
 </template>
 
 <script>
+import CardsContainer from '@/components/CardsContainer.vue'
 export default {
+    components: {
+        CardsContainer
+    },
     props: {
         cards: Array
     }
@@ -25,5 +22,29 @@ export default {
 
 <style lang="scss" scoped>
 @import '@/style/variables.scss';
-@import '@/style/main-container.scss';
+main {
+  background-color: $second-txt-bg-color;
+  color: $first-txt-color;
+    .section-title {
+        position: relative;
+        top: -25px;
+        left: 200px;
+        width: 300px;
+        padding: 10px;
+        background-color: $third-txt-bg-color;
+        text-transform: uppercase;
+        text-align: center;
+    }
+    .button-cards {
+        text-align: center;
+        padding: 20px;
+        button {
+            padding: 10px 50px;
+            border: none;
+            background-color: #0282f9;
+            color: $first-txt-color;
+            cursor: pointer;
+        }
+    }
+}
 </style>
