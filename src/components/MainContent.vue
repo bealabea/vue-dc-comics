46eb30a64322321496f3bef299bac3b5b5d30556
bealabea <beatricemarin89@yahoo.it>
2022-01-28 15:26:37 +0100

    Adds Cards in MainContent

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,21 +1,29 @@
 <template>
-    <main>--content goes here--</main>
+    <main>
+        <div class="section-title"><h1>current series</h1></div>
+        <div class="main-container">
+            <div v-for="(card, index) in cards" :key="index" class="cards">
+                <div class="card-image">
+                    <img :src="card.thumb" alt="">
+                </div>
+                <div>{{card.series}}</div>
+            </div> 
+        </div>
+        <div class="button-cards">
+            <button> LOAD MORE</button>
+        </div>
+    </main>
 </template>
 
 <script>
 export default {
-    
+    props: {
+        cards: Array
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 @import '@/style/variables.scss';
-main {
-    height: 200px;
-    background-color: $second-txt-bg-color;
-    color: $first-txt-color;
-    font-size: 30px;
-    line-height: 200px;
-    padding-left: 50px;
-}
+@import '@/style/main-container.scss';
 </style>
